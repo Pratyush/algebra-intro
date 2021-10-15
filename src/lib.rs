ef96fce079c3c2965fc446d647a340bf6e7df2b1
Pratyush Mishra <pratyushmishra@berkeley.edu>
2021-10-15 07:40:52 -1000

    Add curves and pairings

--- src/lib.rs
+++ src/lib.rs
@@ -104,5 +104,82 @@
 //! let n = F::from_le_bytes_mod_order(&modulus.to_bytes_le());
 //! assert_eq!(n, F::zero());
 //! ```
+//! ## Elliptic curve arithmetic
+//! 
+//! There are two traits that are important when working with elliptic curves
+//! over finite fields: [`ProjectiveCurve`], and [`AffineCurve`]. Both traits
+//! represent the same curve, but provide different underlying representations.
+//! In particular, a [`ProjectiveCurve`] representation of a curve point is generally
+//! more efficient for arithmetic, but does not provide a unique representative
+//! for a curve point. An [`AffineCurve`] representation, on the other hand, *is* unique,
+//! but is slower for most arithmetic operations. Let's explore how and when to use
+//! these:
+//! 
+//! ```rust
+//! use ark_ec::{ProjectiveCurve, AffineCurve};
+//! use ark_ff::{PrimeField, Field};
+//! // We'll use the BLS12-381 G1 curve for this example.
+//! use ark_bls12_381::{G1Projective as G, G1Affine as GAffine, Fr as ScalarField};
+//! use ark_std::{Zero, UniformRand};
+//! 
+//! let mut rng = ark_std::rand::thread_rng();
+//! // Let's sample uniformly random field elements:
+//! let a = G::rand(&mut rng);
+//! let b = G::rand(&mut rng);
+//! 
+//! // We can add...
+//! let c = a + b;
+//! // ... subtract ...
+//! let d = a - b;
+//! // ... and double elements.
+//! assert_eq!(c + d, a.double());
+//! // We can also negate elements...
+//! let e = -a;
+//! assert_eq!(e + a, G::zero());
+//! 
+//! // ...and multiply group elements by elements of the corresponding scalar field
+//! let scalar = ScalarField::rand(&mut rng);
+//! let e = c.mul(&scalar.into_repr()); // into_repr() converts the scalar into a `BigInteger`.
+//! let f = e.mul(&scalar.inverse().unwrap().into_repr());
+//! assert_eq!(f, c);
+//! 
+//! // Finally, we can also convert curve points in projective coordinates to affine coordinates.
+//! let c_aff = c.into_affine();
+//! // Most group operations are slower in affine coordinates, but adding an affine point 
+//! // to a projective one is slightly more efficient.
+//! let d = c.add_mixed(&c_aff);
+//! assert_eq!(d, c.double());
+//! 
+//! // This efficiency also translates into more efficient scalar multiplication routines.
+//! let e_from_aff = c_aff.mul(scalar.into_repr());
+//! assert_eq!(e, e_from_aff);
+//! ```
+//! ## Pairings
+//! 
+//! `PairingEngine` is the primary trait for working with pairings. It contains 
+//! associated types and methods that are relevant to pairing operations:
+//! 
+//! ```rust
+//! use ark_ec::{ProjectiveCurve, AffineCurve, PairingEngine};
+//! use ark_ff::{PrimeField, Field};
+//! // We'll use the BLS12-381 pairing-friendly group for this example.
+//! use ark_bls12_381::{Bls12_381, G1Projective as G1, G2Projective as G2, G1Affine, G2Affine, Fr as ScalarField};
+//! use ark_std::{Zero, UniformRand};
+//! 
+//! let mut rng = ark_std::rand::thread_rng();
+//! // Let's sample uniformly random field elements:
+//! let a: G1Affine = G1::rand(&mut rng).into();
+//! let b: G2Affine = G2::rand(&mut rng).into();
+//! // We can compute the pairing of `a` and `b`:
+//! let c = Bls12_381::pairing(a, b);
+//! 
+//! // We can also compute the pairing partwise:
+//! // First, we compute the Miller loop:
+//! let c_ml = Bls12_381::miller_loop(&[(a.into(), b.into())]);
+//! let c_fe = Bls12_381::final_exponentiation(&c_ml).unwrap();
+//! assert_eq!(c, c_fe);
+//! ```
+
 
 pub use ark_ff::{Field, SquareRootField, PrimeField};
+pub use ark_ec::{AffineCurve, ProjectiveCurve, PairingEngine};
